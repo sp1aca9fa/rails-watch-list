07772ac8d8e7078f6c6a60c1ef9ecd630da84e60
Raphael Barbosa <sp1aca9fa@gmail.com>
2026-02-19 17:05:09 +0900

    functionality done, no visuals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "bookmarks/new"
+  get "lists/index"
+  get "lists/show"
+  get "lists/new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +14,13 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new", as: :new_list
+  get "lists/:id", to: "lists#show", as: :list
+  post "lists", to: "lists#create"
+
+  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
+  post "lists/:id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+  delete "bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
 end
